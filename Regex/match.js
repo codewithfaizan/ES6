dcc7f3e264cdbe42830a19650f0d965dcb3e5029
codewithfaizan <faizanoic.com@gmail.com>
2023-03-31 09:51:07 +0530

    regex

---
+++ Regex/match.js
@@ -0,0 +1,59 @@
+// let name = "Mohammed faizan"
+// let search = /faizan/
+// result = search.test(name);
+//          /regex/.test(nameOftheString)
+// console.log(result)      //true 
+// ---------------------------------------
+// let result = 'Hello sir'
+// console.log(/Hello/.test(result))
+// --------------------------------------
+
+// let testStr = "hello, my name is Kevin";
+// let testRegx = /Kevin/;
+// let notFound = /KEVIN/
+
+// result1 = testRegx.test(testStr)
+// result2 = notFound.test(testStr)
+// // console.log(result1)
+// console.log(result2)
+
+// ----------------------------------------------------
+
+// let result = "Suhail Roushan from Lards".match(/Lards/)
+// console.log(result)
+
+// console.table(result)
+
+// ---------------------------------------------------
+
+// console.log("hello Wrld!".match(/hello/))
+
+// let ourStr = "Regular expression";
+// let ourRegx = /expression/
+// console.log(ourStr.match(ourRegx))
+        // / string.match(regex)
+// console.table(ourStr.match(ourRegx))
+
+// console.log(result)
+// --------------------------------------------------
+
+let extractStr = "Extract the word 'coding' from this";
+let codingRegex = /coding/;
+
+let result = extractStr.match(codingRegex)
+             //   // nameOftheString.match(/regex/)
+console.log(result)
+// console.clear()
+
+// --------------------------------------------------
+
+let regex= /madam/;
+let string = "Hwllo!Good Morning madam";
+console.log(regex.test(string))
+
+// --------------------------------------------------
+// let regex= /madam/;
+// let string = "Hello! Good Morning madam";
+
+// console.log(string.match(regex));
+// ---------------------------------------------------
