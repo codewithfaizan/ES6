bfe23e6ef9f0270ff7128ea383381407fcbdbd58
codewithfaizan <faizanoic.com@gmail.com>
2023-04-02 14:24:20 +0530

    array methods

--- Arrays/array.js
+++ Arrays/array.js
@@ -89,3 +89,4 @@
 // easily check for the presence of an element on an array. indexOf() takes an element as a parameter, 
 // and when called, it returns the position, or index, of that element, or -1 if the element does not exist on the array.
 
+s
