1f9e529d61e11441a3518b4a2521043038c41f70
codewithfaizan <faizanoic.com@gmail.com>
2023-04-11 23:50:07 +0530

    regex

--- Regex/match.js
+++ Regex/match.js
@@ -203,7 +203,18 @@
 // console.log(result)
 // ----------------------------------------------------------------------
 
-let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /\S/g; // Change this line
-let result = sample.match(countNonWhiteSpace);
-console.log(result)
+// let sample = "Whitespace is important in separating words";
+// let countNonWhiteSpace = /\S/g; // Change this line
+// let result = sample.match(countNonWhiteSpace);
+// console.log(result)
+
+// You can specify the lower and upper number of patterns with quantity specifiers.
+// Quantity specifiers are used with curly brackets ({ and }). 
+// You put two numbers between the curly brackets - for the lower and upper number of patterns.
+
+// For example, to match only the letter a appearing between 3 and 5 times in the string ah, 
+// your regex would be /a{3,5}h/.-----------------------------------------------------------------------
+
+// let ohStr = "Ohhh no";
+// let ohRegex = /Oh{3,6}\sno/; // Change this line
+// let result = ohRegex.test(ohStr);
