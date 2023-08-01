0811e54b7b56e6a8b0c0a459d98961e525debc6d
codewithfaizan <faizanoic.com@gmail.com>
2023-08-01 21:39:09 +0530

    scope

--- scope/let.js
+++ scope/let.js
@@ -1,4 +1,38 @@
-let faizan = (sayName) => {
-    return `gitlog ${sayName}`
-}
-console.log(faizan('faizan'))
+// const a = 10 ;
+// console.log("1",a);
+
+// function add() {
+//     // a = a+10
+//     console.log("2",a)
+// }
+// add()
+
+// console.log(a)
+
+// const orders = [
+//     { quantity: 2, item: { name: 'T-Shirt', price: 25 } },
+//     { quantity: 1, item: { name: 'Keyboard', price: 75 } },
+//     { quantity: 2, item: null }
+// ];
+
+// console.log();
+// console.table();
+// console.info();
+
+
+// console.group("class");
+
+// console.log("1")
+// console.log("2")
+// console.log("3")
+
+// // console.groupEnd();
+// console.log("10")
+
+// const {log : sakina } = console
+
+// sakina("hello")
+
+let student = [ 1,2,3.4,5,4 ];
+
+console.table(student)
